eef75fd8d45f4d88338592d1449f519efc4c5a4f
Nenad Radulovic <popcornsftw2011@hotmail.com>
2020-12-10 11:16:58 +0100

    #LAGU-33 Register logic into service. Redirect user

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,48 +1,93 @@
 <template>
-    <div>
-        <form v-on:submit.prevent='onRegister'>
-            <div>
-                <label for="email">Email: </label>
-                <input type="email" name="email" id="email" placeholder="Enter email" required v-model="email">
-            </div>
-            <div><label for="fullname">Fullname: <input type="text" name="fullname" id="fullname" v-model="fullname" placeholder="Enter fullname"></label></div>
-            <div><label for="password">Password: <input type="password" name="password" id="password" required v-model="password" placeholder="Enter password"></label></div>
-            <div><label for="confirm-password">Confirm Password: <input type="password" name="confirm-password" id="confirm-password" v-model="confirmPassword" placeholder="Confirm password"></label></div>
-            <div><button type="submit">Register</button></div>
-        </form>
-    </div>
+  <div>
+    <form v-on:submit.prevent="onRegister">
+      <div>
+        <label for="email">Email: </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter email"
+          required
+          v-model="email"
+        />
+      </div>
+      <div>
+        <label for="fullname"
+          >Fullname:
+          <input
+            type="text"
+            name="fullname"
+            id="fullname"
+            v-model="fullname"
+            placeholder="Enter fullname"
+            required
+        /></label>
+      </div>
+      <div>
+        <label for="password"
+          >Password:
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            v-model="password"
+            placeholder="Enter password"
+        /></label>
+      </div>
+      <div>
+        <label for="confirm-password"
+          >Confirm Password:
+          <input
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+            v-model="confirmPassword"
+            placeholder="Confirm password"
+            required
+        /></label>
+        <p>{{ errors['passwordError'] }}</p>
+      </div>
+      <div><button type="submit">Register</button></div>
+    </form>
+  </div>
 </template>
 
 <script>
-import Axios from 'axios'
-import {url} from '../router/urlConfig.js'
+import { Register } from "../service/AuthService.js";
 export default {
-    data(){
-        return{
-            email: '',
-            fullname: '',
-            password: '',
-            confirmPassword: '',
-        }
+  data() {
+    return {
+      email: "",
+      fullname: "",
+      password: "",
+      confirmPassword: "",
+      errors: {
+          'passwordError' : ''
+      },
+    };
+  },
+  methods: {
+    onRegister() {
+      if (this.Validated()) {
+        Register({
+          email: this.email,
+          name: this.fullname,
+          password: this.password,
+        });
+      }
     },
-    methods: {
-        onRegister(){
-            let payload = {
-                email: this.email,
-                name: this.fullname,
-                password: this.password
-            }
-            Axios.post(url+'/api/user',payload)
-                .then(response => {
-                    console.log(response);
-                }).catch(err => {
-                    console.log(err);
-                })
-        }
-    }
-}
+    Validated() {
+      var valid = true;
+      if (this.password !== this.confirmPassword) {
+        this.errors['passwordError'] = 'Passwords dont match'
+        valid = false;
+      }
+      return valid;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,17 +1,37 @@
 import axios from 'axios'
 import { url } from '../router/urlConfig.js'
+import router from '../router/index.js'
 
 export const Login = (credentials) => {
-
   axios.post(url + '/auth/login', credentials)
     .then(response => {
       setToken(response.data['access_token'])
+      router.push({name: 'Home'})
     })
     .catch(err => {
       console.log(err)
     });
 }
 
+export const Register = (userData) => {
+  axios.post(url + '/api/user', userData)
+    .then(response => {
+      if(response.status == 201){
+        Login({
+          email: userData.email,
+          password: userData.password
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
 const setToken = (token) => {
   window.localStorage.setItem('jwtToken', token)
+}
+
+export const getToken = () => {
+  return window.localStorage.getItem('jwtToken')
 }
